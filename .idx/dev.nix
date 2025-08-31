a40245a1192b26a3c9d806cb596fded8af7200a2
travis.borer <travis.borer@gmail.com>
2025-08-31 20:00:59 +0000

    additional dev.nix fixes

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,8 +6,9 @@
     pkgs.gradle
     pkgs.kotlin
     pkgs.jdk17
-    # Replaced pkgs.android-studio with the more lightweight and appropriate android-sdk
+    # Keep android-sdk and add android-tools for additional utilities
     pkgs.android-sdk
+    pkgs.android-tools
   ];
 
   env = {
@@ -16,24 +17,36 @@
     # Correctly set ANDROID_HOME and ANDROID_SDK_ROOT
     ANDROID_HOME = "${pkgs.android-sdk}";
     ANDROID_SDK_ROOT = "${pkgs.android-sdk}";
+    # Add Android SDK tools to PATH
+    PATH = "${pkgs.android-sdk}/cmdline-tools/latest/bin:${pkgs.android-sdk}/platform-tools:$PATH";
   };
 
   idx = {
     extensions = [
       # Corrected Kotlin extension ID
       "fwcd.kotlin"
-      # Corrected Android extension ID
-      "ms-android.android-pack"
+      # Alternative Android extension (ms-android.android-pack might not be available)
+      "vscjava.vscode-android"
     ];
 
     workspace = {
       onCreate = {
         # Accept Android SDK licenses using the correct sdkmanager path
-        accept-licenses = "yes | ${pkgs.android-sdk}/cmdline-tools/latest/bin/sdkmanager --licenses";
-        # Install required SDK components
-        install-sdk = "${pkgs.android-sdk}/cmdline-tools/latest/bin/sdkmanager "platform-tools" "platforms;android-34" "build-tools;34.0.0"";
+        accept-licenses = ''
+          yes | ${pkgs.android-sdk}/cmdline-tools/latest/bin/sdkmanager --licenses || true
+        '';
+        # Install required SDK components INCLUDING NDK
+        install-sdk = ''
+          ${pkgs.android-sdk}/cmdline-tools/latest/bin/sdkmanager \
+            "platform-tools" \
+            "platforms;android-34" \
+            "build-tools;34.0.0" \
+            "ndk;25.1.8937393" || true
+        '';
         # Setup gradle wrapper
-        gradle-wrapper = "cd app && ./gradlew wrapper || gradle wrapper";
+        gradle-wrapper = ''
+          cd app && (./gradlew wrapper || gradle wrapper) || true
+        '';
       };
       
       onStart = {
