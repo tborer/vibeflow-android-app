5672c1c3aef787ffe4256899d3f463603cc8f349
travis.borer <travis.borer@gmail.com>
2025-08-31 22:50:52 +0000

    fixes

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,13 +2,19 @@
   # Add your Nix packages here
   packages = [
     pkgs.unzip # For unzipping the Android SDK
+    pkgs.wget  # For downloading the Android SDK
   ];
 
   # Add environment variables here
   env = {
     GEMINI_API_KEY = "AIzaSyCuTGRqe5qurdhZiFivq2ovv0tGLuaQeaU";
     ANDROID_HOME = "/home/user/android-sdk";
-    PATH = "$PATH:/home/user/android-sdk/cmdline-tools/latest/bin:/home/user/android-sdk/platform-tools:/home/user/android-sdk/build-tools/34.0.0";
+    # Fix: PATH should be a list, not a string
+    PATH = [
+      "/home/user/android-sdk/cmdline-tools/latest/bin"
+      "/home/user/android-sdk/platform-tools" 
+      "/home/user/android-sdk/build-tools/34.0.0"
+    ];
   };
 
   # Set up VS Code extensions
@@ -22,7 +28,7 @@
       # Runs when a workspace is first created
       onCreate = {
         # Download and set up the Android SDK
-        sdk-setup = ''
+        sdk-setup = '''
           mkdir -p $HOME/android-sdk
           wget -q https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
           unzip -q commandlinetools-linux-11076708_latest.zip -d $HOME/android-sdk/cmdline-tools
@@ -30,7 +36,7 @@
           # Accept all licenses
           yes | $HOME/android-sdk/cmdline-tools/latest/bin/sdkmanager --licenses
           $HOME/android-sdk/cmdline-tools/latest/bin/sdkmanager "build-tools;34.0.0" "platforms;android-34"
-        '';
+        ''';
       };
     };
   };
