f0497a68d3bd9ed2fcc01585e025ad25f1dd2ae5
travis.borer <travis.borer@gmail.com>
2025-08-31 22:42:27 +0000

    fixes

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,4 +1,3 @@
-
 { pkgs, ... }: {
   # Add your Nix packages here
   packages = [
@@ -8,6 +7,8 @@
   # Add environment variables here
   env = {
     GEMINI_API_KEY = "AIzaSyCuTGRqe5qurdhZiFivq2ovv0tGLuaQeaU";
+    ANDROID_HOME = "/home/user/android-sdk";
+    PATH = "$PATH:/home/user/android-sdk/cmdline-tools/latest/bin:/home/user/android-sdk/platform-tools:/home/user/android-sdk/build-tools/34.0.0";
   };
 
   # Set up VS Code extensions
@@ -21,7 +22,7 @@
       # Runs when a workspace is first created
       onCreate = {
         # Download and set up the Android SDK
-        sdk-setup = ''
+        sdk-setup = '''
           mkdir -p $HOME/android-sdk
           wget -q https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
           unzip -q commandlinetools-linux-11076708_latest.zip -d $HOME/android-sdk/cmdline-tools
@@ -29,16 +30,7 @@
           # Accept all licenses
           yes | $HOME/android-sdk/cmdline-tools/latest/bin/sdkmanager --licenses
           $HOME/android-sdk/cmdline-tools/latest/bin/sdkmanager "build-tools;34.0.0" "platforms;android-34"
-        '';
-      };
-
-      # Runs every time the workspace is (re)started
-      onStart = {
-        # Set Android environment variables
-        android-env = ''
-          export ANDROID_HOME=$HOME/android-sdk
-          export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools/34.0.0
-        '';
+        ''';
       };
     };
   };
