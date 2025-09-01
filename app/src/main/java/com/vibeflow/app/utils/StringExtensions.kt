146667aa71a512e41994ac5b486bfd6874b3b421
travis.borer <travis.borer@gmail.com>
2025-09-01 16:16:43 +0000

    removed unused utils

--- app/src/main/java/com/vibeflow/app/utils/StringExtensions.kt
+++
@@ -1,5 +0,0 @@
-package com.vibeflow.app.utils
-
-fun String.reverseString(): String {
-    return this.reversed()
-}
--- app/src/main/java/com/vibeflow/app/utils/Utils.kt
+++
@@ -1,5 +0,0 @@
-package com.vibeflow.app.utils
-
-fun isStringEmpty(text: String?): Boolean {
-    return text.isNullOrEmpty()
-}
