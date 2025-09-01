19bf9f1ade8be39614eaf1a0b6524fe5600389f3
travis.borer <travis.borer@gmail.com>
2025-09-01 16:05:00 +0000

    dev.nix fixes

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -3,18 +3,13 @@
   packages = [
     pkgs.unzip # For unzipping the Android SDK
     pkgs.wget  # For downloading the Android SDK
+    pkgs.jdk   # Java Development Kit for building the app
   ];
 
   # Add environment variables here
   env = {
     GEMINI_API_KEY = "AIzaSyCuTGRqe5qurdhZiFivq2ovv0tGLuaQeaU";
     ANDROID_HOME = "/home/user/android-sdk";
-    # Fix: PATH should be a list, not a string
-    PATH = [
-      "/home/user/android-sdk/cmdline-tools/latest/bin"
-      "/home/user/android-sdk/platform-tools" 
-      "/home/user/android-sdk/build-tools/34.0.0"
-    ];
   };
 
   # Set up VS Code extensions
@@ -28,7 +23,7 @@
       # Runs when a workspace is first created
       onCreate = {
         # Download and set up the Android SDK
-        sdk-setup = '''
+        sdk-setup = ''
           mkdir -p $HOME/android-sdk
           wget -q https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
           unzip -q commandlinetools-linux-11076708_latest.zip -d $HOME/android-sdk/cmdline-tools
@@ -36,7 +31,7 @@
           # Accept all licenses
           yes | $HOME/android-sdk/cmdline-tools/latest/bin/sdkmanager --licenses
           $HOME/android-sdk/cmdline-tools/latest/bin/sdkmanager "build-tools;34.0.0" "platforms;android-34"
-        ''';
+        '';
       };
     };
   };
