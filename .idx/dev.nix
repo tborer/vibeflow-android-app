4fad72646a9ccc9cb3c30fe908dd76ab1ec7339e
travis.borer <travis.borer@gmail.com>
2025-09-01 20:24:59 +0000

    build error fixes

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -9,9 +9,9 @@
   # Add environment variables here
   env = {
     GEMINI_API_KEY = "AIzaSyCuTGRqe5qurdhZiFivq2ovv0tGLuaQeaU";
-    ANDROID_HOME = "${pkgs.stdenv.cc.bintools.x86_64-linux.linker}/bin";
-    ANDROID_SDK_ROOT = "${pkgs.stdenv.cc.bintools.x86_64-linux.linker}/bin";
-    PATH = "$PATH:${pkgs.stdenv.cc.bintools.x86_64-linux.linker}/bin/cmdline-tools/latest/bin:${pkgs.stdenv.cc.bintools.x86_64-linux.linker}/bin/platform-tools";
+    ANDROID_HOME = "/home/user/android-sdk";
+    ANDROID_SDK_ROOT = "/home/user/android-sdk";
+    PATH = "$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools";
   };
 
   # Set up VS Code extensions
@@ -19,42 +19,42 @@
     extensions = [
       "google.gemini"
     ];
-  };
 
-  # Workspace lifecycle hooks
-  workspace = {
-    onCreate = {
-      # Download and unzip the Android SDK
-      install-sdk = ''
-        mkdir -p $ANDROID_HOME
-        cd $ANDROID_HOME
-        wget https://dl.google.com/android/repository/commandlinetools-linux-10406996_latest.zip
-        unzip commandlinetools-linux-10406996_latest.zip
-        rm commandlinetools-linux-10406996_latest.zip
-        mkdir -p $ANDROID_HOME/cmdline-tools/latest
-        mv cmdline-tools/* $ANDROID_HOME/cmdline-tools/latest/
-      '';
-      # Accept licenses
-      accept-licenses = "yes | $ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager --licenses";
-      # Install platform-tools
-      install-platform-tools = "$ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager "platform-tools"";
-    };
-    onStart = {
-      # Grant execute permissions to gradlew
-      gradle-permissions = "chmod +x ./gradlew";
-      # Start the app
-      start-app = "./gradlew run";
-    };
-  };
+      # Workspace lifecycle hooks
+      workspace = {
+        onCreate = {
+          # Download and unzip the Android SDK
+          install-sdk = ''
+            mkdir -p $ANDROID_HOME
+            cd $ANDROID_HOME
+            wget https://dl.google.com/android/repository/commandlinetools-linux-10406996_latest.zip
+            unzip commandlinetools-linux-10406996_latest.zip
+            rm commandlinetools-linux-10406996_latest.zip
+            mkdir -p $ANDROID_HOME/cmdline-tools/latest
+            mv cmdline-tools/* $ANDROID_HOME/cmdline-tools/latest/
+          '';
+          # Accept licenses
+          accept-licenses = "yes | $ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager --licenses";
+          # Install platform-tools
+          install-platform-tools = ''$ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager "platform-tools"'';
+        };
+        onStart = {
+          # Grant execute permissions to gradlew
+          gradle-permissions = "chmod +x ./gradlew";
+          # Start the app
+          start-app = "./gradlew assembleDebug";
+        };
+      };
 
-  # Previews
-  previews = {
-    enable = true;
-    previews = {
-      web = {
-        command = ["./gradlew" "run"];
-        manager = "web";
+      # Previews
+      previews = {
+        enable = true;
+        previews = {
+          web = {
+            command = ["./gradlew" "assembleDebug"];
+            manager = "web";
+          };
+        };
       };
     };
-  };
 }
